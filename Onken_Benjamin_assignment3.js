1f32adb393b29e3fe33400256e7870e30cc37c2e
Ben Onken <BenOnken@Bens-MacBook-Pro.local>
2012-08-15 19:03:52 -0500

    Initial commit with part of JSON done.

---
+++ Onken_Benjamin_assignment3.js
@@ -0,0 +1,30 @@
+/*
+
+
+
+*/
+
+var jsonData = 
+{
+	"monsters": 
+	[
+		{
+			"mID": "01897",
+			"name": "Fire Breathing Belcher",
+			"affix": "Fire Breath"
+		},
+		{
+			"mID": "01898",
+			"name": "Thorntail Chicken Snout",
+			"affix": "Reflects Damage"
+		},
+		{
+			"mID": "01899",
+			"name": "Lightning Lizard of Peace",
+			"affix": "Deception"  
+		},
+	]
+	
+
+
+}
